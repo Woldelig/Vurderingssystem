955de77ef7ffbcbb33e74362d2b8499f4a8feabf
Woldelig <christoffer.wold@gmail.com>
2018-02-01 16:32:39 +0100

    sql editor
    select spørringer fungerer utmerket.
    
    fjernet mulighet for andre spørringer foreløpig, mye validering som må inn her. Design med datagridview trenger en liten overhal også (hvis det går)

--- adminPanel/adminPanel/SqlEditor.cs
+++ adminPanel/adminPanel/SqlEditor.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace adminPanel
 {
     public partial class SqlEditor : UserControl
     {
+        Database db = new Database();
         public SqlEditor()
         {
             InitializeComponent();
+        }
+
+        private void sqlBtn_Click(object sender, EventArgs e)
+        {
+            String connString = "server=localhost;user=root;database=vurderingssystem;";//OBS OBS! HUSK Å ENDRE DATABSEN!
+            MySqlConnection dbConn = new MySqlConnection(connString);
+            String sql = sqlTxt.Text;
+            String[] fyOrd = { "DELETE", "TRUNCATE", "DROP", "INSERT", "UPDATE", "ALTER", "--" }; //Ord som vi ikke vil ha i spørringen
+       
+            foreach (string ord in fyOrd)
+            {
+                if (sql.ToUpperInvariant().Contains(ord.ToString()))
+                {
+                    feilmeldingTxt.ForeColor = Color.Red;
+                    feilmeldingTxt.Text = "SQL spørring er ikke godkjent";
+                    return;
+                }
+            }
+            dbConn.Open();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(sql, dbConn);
+            MySqlCommandBuilder sqlBygger = new MySqlCommandBuilder(da);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            sqlDatagrid.DataSource = ds.Tables[0];
+
+            dbConn.Close();
+
+
         }
     }
 }
