e78149ea8d313c04a65c1f54b0fd1adc7ea1ffa8
Woldelig <christoffer.wold@gmail.com>
2018-02-03 00:05:11 +0100

    kommet i gang !

--- adminPanel/adminPanel/Stats.Designer.cs
+++ adminPanel/adminPanel/Stats.Designer.cs
@@ -28,32 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.fagkodeListeboks = new System.Windows.Forms.ListBox();
+            this.spmListeboks = new System.Windows.Forms.ListBox();
+            this.diagramListeboks = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // label1
+            // fagkodeListeboks
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(476, 276);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Se Statistikk";
+            this.fagkodeListeboks.FormattingEnabled = true;
+            this.fagkodeListeboks.Location = new System.Drawing.Point(67, 55);
+            this.fagkodeListeboks.Name = "fagkodeListeboks";
+            this.fagkodeListeboks.Size = new System.Drawing.Size(120, 108);
+            this.fagkodeListeboks.TabIndex = 0;
+            this.fagkodeListeboks.SelectedIndexChanged += new System.EventHandler(this.fagkodeListeboks_SelectedIndexChanged);
+            // 
+            // spmListeboks
+            // 
+            this.spmListeboks.FormattingEnabled = true;
+            this.spmListeboks.Location = new System.Drawing.Point(247, 55);
+            this.spmListeboks.Name = "spmListeboks";
+            this.spmListeboks.Size = new System.Drawing.Size(120, 108);
+            this.spmListeboks.TabIndex = 1;
+            this.spmListeboks.SelectedIndexChanged += new System.EventHandler(this.spmListeboks_SelectedIndexChanged);
+            // 
+            // diagramListeboks
+            // 
+            this.diagramListeboks.FormattingEnabled = true;
+            this.diagramListeboks.Location = new System.Drawing.Point(427, 55);
+            this.diagramListeboks.Name = "diagramListeboks";
+            this.diagramListeboks.Size = new System.Drawing.Size(120, 108);
+            this.diagramListeboks.TabIndex = 2;
+            this.diagramListeboks.SelectedIndexChanged += new System.EventHandler(this.diagramListeboks_SelectedIndexChanged);
             // 
             // Stats
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.diagramListeboks);
+            this.Controls.Add(this.spmListeboks);
+            this.Controls.Add(this.fagkodeListeboks);
             this.Name = "Stats";
             this.Size = new System.Drawing.Size(1008, 627);
+            this.Load += new System.EventHandler(this.Stats_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox fagkodeListeboks;
+        private System.Windows.Forms.ListBox spmListeboks;
+        private System.Windows.Forms.ListBox diagramListeboks;
     }
 }
--- adminPanel/adminPanel/Stats.cs
+++ adminPanel/adminPanel/Stats.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace adminPanel
 {
@@ -15,6 +16,74 @@ namespace adminPanel
         public Stats()
         {
             InitializeComponent();
+        }
+        private static String connString = "server=localhost;user=root;database=vurderingssystem;";//OBS OBS! HUSK Å ENDRE DATABSEN!
+        MySqlConnection dbConn = new MySqlConnection(connString);
+
+
+        private void Stats_Load(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT fagkode FROM fag;", dbConn);
+            dbConn.Open(); //åpne
+
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds);
+            dt = ds.Tables[0]; //plassering 0 fordi den kun henter ut en rad. i foreachen blir den splittet opp per rad og lagt inn en og en i listeboksen
+
+            foreach (DataRow dr in dt.Rows)  //Her brukes datarow fordi vi skal ha ut rader
+            {
+                fagkodeListeboks.Items.Add(dr["Fagkode"].ToString());
+            }
+            dbConn.Close(); //lukke. Burde de åpnes/lukkes for vær gang? Spørr lasse https://softwareengineering.stackexchange.com/questions/142065/creating-database-connections-do-it-once-or-for-each-query
+
+            spmListeboks.Hide();    //Gjemmer listeboksene til vi har data i de
+            diagramListeboks.Hide();
+        }
+
+        private void fagkodeListeboks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dbConn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT spm1, spm2, spm3, spm4, spm5, spm6, spm7, spm8, spm9, spm10 FROM vurderingshistorikk;", dbConn);
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds);
+            dt = ds.Tables[0]; //plassering 0 fordi den kun henter ut en rad. i foreachen blir den splittet opp per rad og lagt inn en og en i listeboksen
+
+            foreach (DataColumn dc in dt.Columns)  //her bruker vi dataColumn fordi vi skal ha ut kolonner
+            {
+                spmListeboks.Items.Add(dc.ToString());
+                Console.WriteLine(dc);
+            }
+            spmListeboks.Show();
+            dbConn.Close();
+
+        }
+
+        private void spmListeboks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            diagramListeboks.Show();
+            String[] diagramTyper = {"Kakediagram", "Stolpediagram" }; //Legg til flere diagrammer her når vi er i gang
+
+            foreach (String diagram in diagramTyper)
+            {
+                diagramListeboks.Items.Add(diagram.ToString());
+            }
+        }
+
+        private void diagramListeboks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (diagramListeboks)
+            {
+                default:
+                    break;
+            }
+
+            //bruke switch case for å sjekke valg i listeboksen. Som starter metode for å tegne diagrammet!
         }
     }
 }
