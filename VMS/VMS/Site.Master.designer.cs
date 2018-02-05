e037fe3a27629433bf9c670269cf7a983aa642a5
Vetle <vetlehaugan@gmail.com>
2018-02-05 09:44:44 +0100

    Gjort knappene om til ASP knapper
    ^

--- VMS/VMS/Site.Master.designer.cs
+++ VMS/VMS/Site.Master.designer.cs
@@ -13,6 +13,24 @@ namespace VMS {
     public partial class SiteMaster {
         
         /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// searchBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton searchBtn;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
