54cfdc4559895ac646956af088a58c2ba350bedb
Woldelig <christoffer.wold@gmail.com>
2018-02-07 13:38:29 +0100

    ræb

--- adminPanel/adminPanel/Stats.cs
+++ adminPanel/adminPanel/Stats.cs
@@ -73,47 +73,56 @@ namespace adminPanel
 
         private void diagramListeboks_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int verdi1 = 20000, verdi2 = 10000, verdi3 = 40000, verdi4 = 20000, verdi5 = 40000; //Denne er her bare for å fjerne rødlinjer. men skal represntere svar i spm 1-5
-
+            MySqlCommand cmd = new MySqlCommand();
             switch (spmListeboks.SelectedIndex) //Her legger vi inn hvilken prosedyre vi kaller på
             {
                 case 0:
+                    cmd.CommandText = "hent_spm1_verdier";
                     Console.WriteLine("spørsmål 1");
                     break;
 
                 case 1:
+                    cmd.CommandText = "hent_spm2_verdier";
                     Console.WriteLine("spørsmål 2");
                     break;
 
                 case 2:
+                    cmd.CommandText = "hent_spm3_verdier";
                     Console.WriteLine("spørsmål 3");
                     break;
 
                 case 3:
+                    cmd.CommandText = "hent_spm4_verdier";
                     Console.WriteLine("spørsmål 4");
                     break;
 
                 case 4:
+                    cmd.CommandText = "hent_spm5_verdier";
                     Console.WriteLine("spørsmål 5");
                     break;
 
                 case 5:
+                    cmd.CommandText = "hent_spm16_verdier";
                     Console.WriteLine("spørsmål 6");
                     break;
 
                 case 6:
+                    cmd.CommandText = "hent_spm7_verdier";
                     Console.WriteLine("spørsmål 7");
                     break;
 
                 case 7:
+                    cmd.CommandText = "hent_spm8_verdier";
                     Console.WriteLine("spørsmål 8");
                     break;
 
                 case 8:
+                    cmd.CommandText = "hent_spm9_verdier";
                     Console.WriteLine("spørsmål 9");
                     break;
 
                 case 9:
+                    cmd.CommandText = "hent_spm10_verdier";
                     Console.WriteLine("spørsmål 10");
                     break;
 
@@ -122,33 +131,46 @@ namespace adminPanel
             }
 
 
-            
-
-            MySqlCommand cmd = new MySqlCommand("hent_spm2_verdier", dbConn);
-            //cmd.CommandText = "prosedyrenavn";
+            cmd.Connection = dbConn;
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add(new MySqlParameter("?out_verdi1", MySqlDbType.VarChar));
-            cmd.Parameters["?out_verdi1"].Direction = ParameterDirection.Output;
-            cmd.Parameters.Add(new MySqlParameter("?out_verdi2", MySqlDbType.Int16));
-            cmd.Parameters["?out_verdi2"].Direction = ParameterDirection.Output;
-            cmd.Parameters.Add(new MySqlParameter("?out_verdi3", MySqlDbType.Int16));
-            cmd.Parameters["?out_verdi3"].Direction = ParameterDirection.Output;
-            cmd.Parameters.Add(new MySqlParameter("?out_verdi4", MySqlDbType.Int16));
-            cmd.Parameters["?out_verdi4"].Direction = ParameterDirection.Output;
-            cmd.Parameters.Add(new MySqlParameter("?out_verdi5", MySqlDbType.Int16));
-            cmd.Parameters["?out_verdi5"].Direction = ParameterDirection.Output;
+            string fagkode = fagkodeListeboks.SelectedItem.ToString();
+            cmd.Parameters.AddWithValue("@in_fagkode", fagkode).Direction = ParameterDirection.Input;
+            //cmd.Parameters["@in_fagkode"].Direction = ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@out_verdi1", MySqlDbType.Int32).Direction = ParameterDirection.Output;
+            //cmd.Parameters["@out_verdi1"].Direction = ParameterDirection.Output;
+            cmd.Parameters.AddWithValue("@out_verdi2", MySqlDbType.Int32).Direction = ParameterDirection.Output;
+            cmd.Parameters.AddWithValue("@out_verdi3", MySqlDbType.Int32).Direction = ParameterDirection.Output;
+            cmd.Parameters.AddWithValue("@out_verdi4", MySqlDbType.Int32).Direction = ParameterDirection.Output;
+            cmd.Parameters.AddWithValue("@out_verdi5", MySqlDbType.Int32).Direction = ParameterDirection.Output;
+
+            Console.WriteLine(cmd.CommandText);
+
+
+
+            Console.WriteLine("Verdi 1 " + cmd.Parameters["@out_verdi1"]);
+            Console.WriteLine("Verdi 1 " + cmd.Parameters[0]);
+            Console.WriteLine("Verdi 1 " + cmd.Parameters["@out_verdi2"]);
+            Console.WriteLine("Verdi 2 " + cmd.Parameters[1]);
+            Console.WriteLine("Verdi 1 " + cmd.Parameters["@out_verdi3"]);
+            Console.WriteLine("Verdi 3 " + cmd.Parameters[2]);
+            Console.WriteLine("Verdi 1 " + cmd.Parameters["@out_verdi4"]);
+            Console.WriteLine("Verdi 4 " + cmd.Parameters[3]);
+            Console.WriteLine("Verdi 5 " + cmd.Parameters["@out_verdi5"]);
+            Console.WriteLine("Verdi 1 " + cmd.Parameters[4]);
 
             dbConn.Open();
-            string stjerne1 = (string)cmd.Parameters["?out_verdi1"].Value;/*
-            int stjerne2 = (int)cmd.Parameters["?out_verdi2"].Value;
-            int stjerne3 = (int)cmd.Parameters["?out_verdi3"].Value;
-            int stjerne4 = (int)cmd.Parameters["?out_verdi4"].Value;
-            int stjerne5 = (int)cmd.Parameters["?out_verdi5"].Value;*/
+            int stjerne1 = (int)cmd.Parameters["@out_verdi1"].Value;
+            int stjerne2 = (int)cmd.Parameters["@out_verdi2"].Value;
+            int stjerne3 = (int)cmd.Parameters["@out_verdi3"].Value;
+            int stjerne4 = (int)cmd.Parameters["@out_verdi4"].Value;
+            int stjerne5 = (int)cmd.Parameters["@out_verdi5"].Value;
+            Console.WriteLine("Verdi 1: " + stjerne1.ToString());
+            Console.WriteLine("Verdi 2: " + stjerne2.ToString());
+            Console.WriteLine("Verdi 3: " + stjerne3.ToString());
+            Console.WriteLine("Verdi 4: " + stjerne4.ToString());
+            Console.WriteLine("Verdi 5: " + stjerne5.ToString());
             cmd.ExecuteNonQuery();
             dbConn.Close();
-
-            
-            Console.WriteLine(stjerne1/*.ToString(), stjerne2.ToString(), stjerne3.ToString(), stjerne4.ToString(), stjerne5.ToString()*/);
 
             chart1.Series.Clear();
             string seriesname = "seriesName"; //Av en eller annen grunn heter den dette overalt på stackoverflow så følger det
@@ -180,11 +202,11 @@ namespace adminPanel
                         break;
                 }
 
-                chart1.Series[seriesname].Points.AddXY("1 Stjerne", verdi1);
-                chart1.Series[seriesname].Points.AddXY("2 Stjerner", verdi2);
-                chart1.Series[seriesname].Points.AddXY("3 Stjerner", verdi3);
-                chart1.Series[seriesname].Points.AddXY("4 Stjerner", verdi4);
-                chart1.Series[seriesname].Points.AddXY("5 Stjerner", verdi5);
+                chart1.Series[seriesname].Points.AddXY("1 Stjerne", stjerne1);
+                chart1.Series[seriesname].Points.AddXY("2 Stjerner", stjerne2);
+                chart1.Series[seriesname].Points.AddXY("3 Stjerner", stjerne3);
+                chart1.Series[seriesname].Points.AddXY("4 Stjerner", stjerne4);
+                chart1.Series[seriesname].Points.AddXY("5 Stjerner", stjerne5);
                 chart1.Show();
             }
             catch (Exception ex)
@@ -194,3 +216,15 @@ namespace adminPanel
         }
     }
 }
+
+/*
+           cmd.Parameters.Add(new MySqlParameter("?out_verdi1", MySqlDbType.Int16));
+           cmd.Parameters["?out_verdi1"].Direction = ParameterDirection.Output;
+           cmd.Parameters.Add(new MySqlParameter("?out_verdi2", MySqlDbType.Int16));
+           cmd.Parameters["?out_verdi2"].Direction = ParameterDirection.Output;
+           cmd.Parameters.Add(new MySqlParameter("?out_verdi3", MySqlDbType.Int16));
+           cmd.Parameters["?out_verdi3"].Direction = ParameterDirection.Output;
+           cmd.Parameters.Add(new MySqlParameter("?out_verdi4", MySqlDbType.Int16));
+           cmd.Parameters["?out_verdi4"].Direction = ParameterDirection.Output;
+           cmd.Parameters.Add(new MySqlParameter("?out_verdi5", MySqlDbType.Int16));
+           cmd.Parameters["?out_verdi5"].Direction = ParameterDirection.Output;*/
