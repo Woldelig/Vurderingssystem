51a75a7ca36bfbc3cb4ba621bcb0735ece00fd82
Woldelig <christoffer.wold@gmail.com>
2018-02-08 23:35:44 +0100

    førsteutkat
    Spørreskjema blir lastet opp uten problem, har validering som er ganske effektiv.
    
    Må få et ekstra par med øyne til å se om vi kan gjøre paramater bindingen bedre og til å se på designet

--- adminPanel/adminPanel/Schema.Designer.cs
+++ adminPanel/adminPanel/Schema.Designer.cs
@@ -28,32 +28,290 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.beskrivelseLbl = new System.Windows.Forms.Label();
+            this.spm1Lbl = new System.Windows.Forms.Label();
+            this.beskrivelseTxt = new System.Windows.Forms.TextBox();
+            this.spm1Txt = new System.Windows.Forms.TextBox();
+            this.spm2Txt = new System.Windows.Forms.TextBox();
+            this.spm2Lbl = new System.Windows.Forms.Label();
+            this.spm3Txt = new System.Windows.Forms.TextBox();
+            this.spm3Lbl = new System.Windows.Forms.Label();
+            this.spm4Txt = new System.Windows.Forms.TextBox();
+            this.spm4Lbl = new System.Windows.Forms.Label();
+            this.spm5Txt = new System.Windows.Forms.TextBox();
+            this.spm5Lbl = new System.Windows.Forms.Label();
+            this.spm6Txt = new System.Windows.Forms.TextBox();
+            this.spm6Lbl = new System.Windows.Forms.Label();
+            this.spm7Txt = new System.Windows.Forms.TextBox();
+            this.spm7Lbl = new System.Windows.Forms.Label();
+            this.spm8Txt = new System.Windows.Forms.TextBox();
+            this.spm8Lbl = new System.Windows.Forms.Label();
+            this.spm9Txt = new System.Windows.Forms.TextBox();
+            this.spm9Lbl = new System.Windows.Forms.Label();
+            this.spm10Txt = new System.Windows.Forms.TextBox();
+            this.spm10Lbl = new System.Windows.Forms.Label();
+            this.lagSkjemaBtn = new System.Windows.Forms.Button();
+            this.resultatLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // beskrivelseLbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(492, 284);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Spørreskjema";
+            this.beskrivelseLbl.AutoSize = true;
+            this.beskrivelseLbl.Location = new System.Drawing.Point(561, 33);
+            this.beskrivelseLbl.Name = "beskrivelseLbl";
+            this.beskrivelseLbl.Size = new System.Drawing.Size(173, 13);
+            this.beskrivelseLbl.TabIndex = 3;
+            this.beskrivelseLbl.Text = "Skriv inn en beskrivelse for skjema:";
+            // 
+            // spm1Lbl
+            // 
+            this.spm1Lbl.AutoSize = true;
+            this.spm1Lbl.Location = new System.Drawing.Point(31, 33);
+            this.spm1Lbl.Name = "spm1Lbl";
+            this.spm1Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm1Lbl.TabIndex = 4;
+            this.spm1Lbl.Text = "Skriv inn spørsmål 1:";
+            // 
+            // beskrivelseTxt
+            // 
+            this.beskrivelseTxt.Location = new System.Drawing.Point(564, 49);
+            this.beskrivelseTxt.Multiline = true;
+            this.beskrivelseTxt.Name = "beskrivelseTxt";
+            this.beskrivelseTxt.Size = new System.Drawing.Size(238, 98);
+            this.beskrivelseTxt.TabIndex = 5;
+            // 
+            // spm1Txt
+            // 
+            this.spm1Txt.Location = new System.Drawing.Point(33, 49);
+            this.spm1Txt.Name = "spm1Txt";
+            this.spm1Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm1Txt.TabIndex = 6;
+            // 
+            // spm2Txt
+            // 
+            this.spm2Txt.Location = new System.Drawing.Point(33, 88);
+            this.spm2Txt.Name = "spm2Txt";
+            this.spm2Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm2Txt.TabIndex = 8;
+            // 
+            // spm2Lbl
+            // 
+            this.spm2Lbl.AutoSize = true;
+            this.spm2Lbl.Location = new System.Drawing.Point(31, 72);
+            this.spm2Lbl.Name = "spm2Lbl";
+            this.spm2Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm2Lbl.TabIndex = 7;
+            this.spm2Lbl.Text = "Skriv inn spørsmål 2:";
+            // 
+            // spm3Txt
+            // 
+            this.spm3Txt.Location = new System.Drawing.Point(33, 127);
+            this.spm3Txt.Name = "spm3Txt";
+            this.spm3Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm3Txt.TabIndex = 10;
+            // 
+            // spm3Lbl
+            // 
+            this.spm3Lbl.AutoSize = true;
+            this.spm3Lbl.Location = new System.Drawing.Point(31, 111);
+            this.spm3Lbl.Name = "spm3Lbl";
+            this.spm3Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm3Lbl.TabIndex = 9;
+            this.spm3Lbl.Text = "Skriv inn spørsmål 3:";
+            // 
+            // spm4Txt
+            // 
+            this.spm4Txt.Location = new System.Drawing.Point(33, 166);
+            this.spm4Txt.Name = "spm4Txt";
+            this.spm4Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm4Txt.TabIndex = 12;
+            // 
+            // spm4Lbl
+            // 
+            this.spm4Lbl.AutoSize = true;
+            this.spm4Lbl.Location = new System.Drawing.Point(31, 150);
+            this.spm4Lbl.Name = "spm4Lbl";
+            this.spm4Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm4Lbl.TabIndex = 11;
+            this.spm4Lbl.Text = "Skriv inn spørsmål 4:";
+            // 
+            // spm5Txt
+            // 
+            this.spm5Txt.Location = new System.Drawing.Point(33, 205);
+            this.spm5Txt.Name = "spm5Txt";
+            this.spm5Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm5Txt.TabIndex = 14;
+            // 
+            // spm5Lbl
+            // 
+            this.spm5Lbl.AutoSize = true;
+            this.spm5Lbl.Location = new System.Drawing.Point(31, 189);
+            this.spm5Lbl.Name = "spm5Lbl";
+            this.spm5Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm5Lbl.TabIndex = 13;
+            this.spm5Lbl.Text = "Skriv inn spørsmål 5:";
+            // 
+            // spm6Txt
+            // 
+            this.spm6Txt.Location = new System.Drawing.Point(33, 244);
+            this.spm6Txt.Name = "spm6Txt";
+            this.spm6Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm6Txt.TabIndex = 16;
+            // 
+            // spm6Lbl
+            // 
+            this.spm6Lbl.AutoSize = true;
+            this.spm6Lbl.Location = new System.Drawing.Point(31, 228);
+            this.spm6Lbl.Name = "spm6Lbl";
+            this.spm6Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm6Lbl.TabIndex = 15;
+            this.spm6Lbl.Text = "Skriv inn spørsmål 6:";
+            // 
+            // spm7Txt
+            // 
+            this.spm7Txt.Location = new System.Drawing.Point(33, 283);
+            this.spm7Txt.Name = "spm7Txt";
+            this.spm7Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm7Txt.TabIndex = 18;
+            // 
+            // spm7Lbl
+            // 
+            this.spm7Lbl.AutoSize = true;
+            this.spm7Lbl.Location = new System.Drawing.Point(31, 267);
+            this.spm7Lbl.Name = "spm7Lbl";
+            this.spm7Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm7Lbl.TabIndex = 17;
+            this.spm7Lbl.Text = "Skriv inn spørsmål 7:";
+            // 
+            // spm8Txt
+            // 
+            this.spm8Txt.Location = new System.Drawing.Point(33, 322);
+            this.spm8Txt.Name = "spm8Txt";
+            this.spm8Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm8Txt.TabIndex = 20;
+            // 
+            // spm8Lbl
+            // 
+            this.spm8Lbl.AutoSize = true;
+            this.spm8Lbl.Location = new System.Drawing.Point(31, 306);
+            this.spm8Lbl.Name = "spm8Lbl";
+            this.spm8Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm8Lbl.TabIndex = 19;
+            this.spm8Lbl.Text = "Skriv inn spørsmål 8:";
+            // 
+            // spm9Txt
+            // 
+            this.spm9Txt.Location = new System.Drawing.Point(33, 361);
+            this.spm9Txt.Name = "spm9Txt";
+            this.spm9Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm9Txt.TabIndex = 22;
+            // 
+            // spm9Lbl
+            // 
+            this.spm9Lbl.AutoSize = true;
+            this.spm9Lbl.Location = new System.Drawing.Point(31, 345);
+            this.spm9Lbl.Name = "spm9Lbl";
+            this.spm9Lbl.Size = new System.Drawing.Size(104, 13);
+            this.spm9Lbl.TabIndex = 21;
+            this.spm9Lbl.Text = "Skriv inn spørsmål 9:";
+            // 
+            // spm10Txt
+            // 
+            this.spm10Txt.Location = new System.Drawing.Point(33, 400);
+            this.spm10Txt.Name = "spm10Txt";
+            this.spm10Txt.Size = new System.Drawing.Size(468, 20);
+            this.spm10Txt.TabIndex = 24;
+            // 
+            // spm10Lbl
+            // 
+            this.spm10Lbl.AutoSize = true;
+            this.spm10Lbl.Location = new System.Drawing.Point(31, 384);
+            this.spm10Lbl.Name = "spm10Lbl";
+            this.spm10Lbl.Size = new System.Drawing.Size(110, 13);
+            this.spm10Lbl.TabIndex = 23;
+            this.spm10Lbl.Text = "Skriv inn spørsmål 10:";
+            // 
+            // lagSkjemaBtn
+            // 
+            this.lagSkjemaBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lagSkjemaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lagSkjemaBtn.Location = new System.Drawing.Point(33, 462);
+            this.lagSkjemaBtn.Name = "lagSkjemaBtn";
+            this.lagSkjemaBtn.Size = new System.Drawing.Size(158, 87);
+            this.lagSkjemaBtn.TabIndex = 25;
+            this.lagSkjemaBtn.Text = "Lag spørreskjema";
+            this.lagSkjemaBtn.UseVisualStyleBackColor = false;
+            this.lagSkjemaBtn.Click += new System.EventHandler(this.lagSkjemaBtn_Click);
+            // 
+            // resultatLbl
+            // 
+            this.resultatLbl.AutoSize = true;
+            this.resultatLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultatLbl.Location = new System.Drawing.Point(235, 462);
+            this.resultatLbl.Name = "resultatLbl";
+            this.resultatLbl.Size = new System.Drawing.Size(0, 25);
+            this.resultatLbl.TabIndex = 26;
             // 
             // Schema
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultatLbl);
+            this.Controls.Add(this.lagSkjemaBtn);
+            this.Controls.Add(this.spm10Txt);
+            this.Controls.Add(this.spm10Lbl);
+            this.Controls.Add(this.spm9Txt);
+            this.Controls.Add(this.spm9Lbl);
+            this.Controls.Add(this.spm8Txt);
+            this.Controls.Add(this.spm8Lbl);
+            this.Controls.Add(this.spm7Txt);
+            this.Controls.Add(this.spm7Lbl);
+            this.Controls.Add(this.spm6Txt);
+            this.Controls.Add(this.spm6Lbl);
+            this.Controls.Add(this.spm5Txt);
+            this.Controls.Add(this.spm5Lbl);
+            this.Controls.Add(this.spm4Txt);
+            this.Controls.Add(this.spm4Lbl);
+            this.Controls.Add(this.spm3Txt);
+            this.Controls.Add(this.spm3Lbl);
+            this.Controls.Add(this.spm2Txt);
+            this.Controls.Add(this.spm2Lbl);
+            this.Controls.Add(this.spm1Txt);
+            this.Controls.Add(this.beskrivelseTxt);
+            this.Controls.Add(this.spm1Lbl);
+            this.Controls.Add(this.beskrivelseLbl);
             this.Name = "Schema";
             this.Size = new System.Drawing.Size(1008, 627);
+            this.Load += new System.EventHandler(this.Schema_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label beskrivelseLbl;
+        private System.Windows.Forms.Label spm1Lbl;
+        private System.Windows.Forms.TextBox beskrivelseTxt;
+        private System.Windows.Forms.TextBox spm1Txt;
+        private System.Windows.Forms.TextBox spm2Txt;
+        private System.Windows.Forms.Label spm2Lbl;
+        private System.Windows.Forms.TextBox spm3Txt;
+        private System.Windows.Forms.Label spm3Lbl;
+        private System.Windows.Forms.TextBox spm4Txt;
+        private System.Windows.Forms.Label spm4Lbl;
+        private System.Windows.Forms.TextBox spm5Txt;
+        private System.Windows.Forms.Label spm5Lbl;
+        private System.Windows.Forms.TextBox spm6Txt;
+        private System.Windows.Forms.Label spm6Lbl;
+        private System.Windows.Forms.TextBox spm7Txt;
+        private System.Windows.Forms.Label spm7Lbl;
+        private System.Windows.Forms.TextBox spm8Txt;
+        private System.Windows.Forms.Label spm8Lbl;
+        private System.Windows.Forms.TextBox spm9Txt;
+        private System.Windows.Forms.Label spm9Lbl;
+        private System.Windows.Forms.TextBox spm10Txt;
+        private System.Windows.Forms.Label spm10Lbl;
+        private System.Windows.Forms.Button lagSkjemaBtn;
+        private System.Windows.Forms.Label resultatLbl;
     }
 }
--- adminPanel/adminPanel/Schema.cs
+++ adminPanel/adminPanel/Schema.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace adminPanel
 {
@@ -15,6 +16,66 @@ namespace adminPanel
         public Schema()
         {
             InitializeComponent();
+        }
+        Database db = new Database();
+
+        private void Schema_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lagSkjemaBtn_Click(object sender, EventArgs e)
+        {
+
+            foreach (Control c in this.Controls) //Denne foreachen sjekker om samtlige tekstbokser er fylt ut. Og avbryter koden hvis ikke
+            {
+                if (c is TextBox)
+                {
+                    TextBox textBox = c as TextBox;
+                    if (textBox.Text == string.Empty)
+                    {
+                        resultatLbl.ForeColor = Color.Red;
+                        resultatLbl.Text = "Fyll ut alle tekstfelt.";
+                        return;
+                    }
+                }
+            }
+            String query = "INSERT INTO vurderingsskjema VALUES (NULL, @Beskrivelse, @Spm1, @Spm2, @Spm3, @Spm4, @Spm5, @Spm6, @Spm7, @Spm8, @Spm9, @Spm10);";
+            var mySqlCommand = db.SqlCommand(query);
+            mySqlCommand.Parameters.AddWithValue("@Beskrivelse", beskrivelseTxt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm1", spm1Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm2", spm2Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm3", spm3Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm4", spm4Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm5", spm5Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm6", spm6Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm7", spm7Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm8", spm8Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm9", spm9Txt.Text);
+            mySqlCommand.Parameters.AddWithValue("@Spm10", spm10Txt.Text);
+            db.OpenConnection();
+
+            try
+            {
+                mySqlCommand.ExecuteNonQuery();
+                resultatLbl.Text = "Spørreskjema er laget";
+
+                foreach (var c in Controls) //denne foreachen vil tømme alle textboksene i formen
+                {
+                    if (c is TextBox)
+                    {
+                        ((TextBox)c).Text = String.Empty;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+            }
+            Console.WriteLine(query);
+            db.CloseConnection();
+
         }
     }
 }
