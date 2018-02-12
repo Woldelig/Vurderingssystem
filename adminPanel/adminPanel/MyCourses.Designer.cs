cb9cf751f3156a41d6c95c778e592ca2397419e7
ZuperNova <trugunn_css@hotmail.com>
2018-02-12 16:09:32 +0100

    Satt opp panel for fagene
    Det settes opp et eget panel som har oversikt over alle fagene til læreren. Alle fagene blir en knapp. Videre er det tenkt at det skal komme flere valgmuligheter når et fag velges

--- adminPanel/adminPanel/MyCourses.Designer.cs
+++ adminPanel/adminPanel/MyCourses.Designer.cs
@@ -28,32 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.MyCoursesHeader = new System.Windows.Forms.Label();
+            this.MyCoursesPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // MyCoursesHeader
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(454, 283);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mine Fag";
+            this.MyCoursesHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MyCoursesHeader.Font = new System.Drawing.Font("Century Gothic", 27.75F);
+            this.MyCoursesHeader.Location = new System.Drawing.Point(0, 0);
+            this.MyCoursesHeader.Name = "MyCoursesHeader";
+            this.MyCoursesHeader.Size = new System.Drawing.Size(1008, 82);
+            this.MyCoursesHeader.TabIndex = 0;
+            this.MyCoursesHeader.Text = "Mine Fag";
+            this.MyCoursesHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MyCoursesPanel
+            // 
+            this.MyCoursesPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.MyCoursesPanel.Location = new System.Drawing.Point(61, 85);
+            this.MyCoursesPanel.Name = "MyCoursesPanel";
+            this.MyCoursesPanel.Size = new System.Drawing.Size(435, 500);
+            this.MyCoursesPanel.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(747, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MyCourses
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MyCoursesPanel);
+            this.Controls.Add(this.MyCoursesHeader);
             this.Name = "MyCourses";
             this.Size = new System.Drawing.Size(1008, 627);
+            this.Load += new System.EventHandler(this.MyCourses_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MyCoursesHeader;
+        private System.Windows.Forms.Panel MyCoursesPanel;
+        private System.Windows.Forms.Button button1;
     }
 }
--- adminPanel/adminPanel/MyCourses.cs
+++ adminPanel/adminPanel/MyCourses.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace adminPanel
 {
@@ -15,6 +16,61 @@ namespace adminPanel
         public MyCourses()
         {
             InitializeComponent();
+        }
+
+        private void MyCourses_Load(object sender, EventArgs e)
+        {
+            Database db = new Database();
+            String sql = "SELECT fagkode FROM fag;";
+            var mySqlCommand = db.SqlCommand(sql);
+            int row = 0; //10
+            int column = 0; //3
+            string fagkode = "";
+
+
+            try
+            {
+                db.OpenConnection();
+                MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        if (column > 2)
+                        {
+                            column = 0;
+                            row++;
+
+                            if (row > 10)
+                            {
+                                row = 0;
+                            }
+                        }
+
+                        fagkode = reader.GetString("fagkode");
+                        Button button = new Button();
+                        button.FlatAppearance.BorderSize = 0;
+                        button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                        button.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        button.Name = fagkode;
+                        button.Text = fagkode;
+                        button.Left = column * 145;
+                        button.Top = row * 50;
+                        button.Width = 145;
+                        button.Height = 50;
+                        MyCoursesPanel.Controls.Add(button);
+                        column++;
+                    }
+                }
+            }
+            catch (MySqlException DBexception)
+            {
+
+                Console.WriteLine("Feilmelding: ", DBexception);
+            }
+            
+
         }
     }
 }
