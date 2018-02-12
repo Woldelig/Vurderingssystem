bbcdcb265b0531a2b937e1c21119b358b2c3cc0c
Vetle <vetlehaugan@gmail.com>
2018-02-12 17:06:05 +0100

    søkbar fiksing
    Gjort så søkeknappen fungerer på andre nettlesere enn Firefox

--- VMS/VMS/Site.Master.designer.cs
+++ VMS/VMS/Site.Master.designer.cs
@@ -22,13 +22,13 @@ namespace VMS {
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
         
         /// <summary>
-        /// searchBtn control.
+        /// Button1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton searchBtn;
+        protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
         /// MainContent control.
