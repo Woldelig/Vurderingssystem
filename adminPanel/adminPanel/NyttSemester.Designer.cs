8bf118dd60b9e8f7f777d886832e36293717c718
Woldelig <christoffer.wold@gmail.com>
2018-03-11 16:03:34 +0100

    byttet navn la til hjelpeknapp
    trenger muligens en bedre forklaring i hjelpeknappen

--- adminPanel/adminPanel/NyttSemester.Designer.cs
+++ adminPanel/adminPanel/NyttSemester.Designer.cs
@@ -28,18 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GodkjennNyttSemesterLbl = new System.Windows.Forms.Label();
+            this.ValideringsTextBox = new System.Windows.Forms.TextBox();
+            this.ValideringsInfoLbl = new System.Windows.Forms.Label();
+            this.HjelpBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 294);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // GodkjennNyttSemesterLbl
+            // 
+            this.GodkjennNyttSemesterLbl.AutoSize = true;
+            this.GodkjennNyttSemesterLbl.Location = new System.Drawing.Point(658, 153);
+            this.GodkjennNyttSemesterLbl.Name = "GodkjennNyttSemesterLbl";
+            this.GodkjennNyttSemesterLbl.Size = new System.Drawing.Size(53, 13);
+            this.GodkjennNyttSemesterLbl.TabIndex = 2;
+            this.GodkjennNyttSemesterLbl.Text = "Godkjenn";
+            // 
+            // ValideringsTextBox
+            // 
+            this.ValideringsTextBox.Location = new System.Drawing.Point(836, 153);
+            this.ValideringsTextBox.Name = "ValideringsTextBox";
+            this.ValideringsTextBox.ReadOnly = true;
+            this.ValideringsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ValideringsTextBox.TabIndex = 3;
+            // 
+            // ValideringsInfoLbl
+            // 
+            this.ValideringsInfoLbl.AutoSize = true;
+            this.ValideringsInfoLbl.Location = new System.Drawing.Point(833, 137);
+            this.ValideringsInfoLbl.Name = "ValideringsInfoLbl";
+            this.ValideringsInfoLbl.Size = new System.Drawing.Size(84, 13);
+            this.ValideringsInfoLbl.TabIndex = 4;
+            this.ValideringsInfoLbl.Text = "Valideringsboks:";
+            // 
+            // HjelpBtn
+            // 
+            this.HjelpBtn.Location = new System.Drawing.Point(45, 46);
+            this.HjelpBtn.Name = "HjelpBtn";
+            this.HjelpBtn.Size = new System.Drawing.Size(114, 38);
+            this.HjelpBtn.TabIndex = 6;
+            this.HjelpBtn.Text = "Hjelp";
+            this.HjelpBtn.UseVisualStyleBackColor = true;
+            this.HjelpBtn.Click += new System.EventHandler(this.HjelpBtn_Click);
             // 
             // NyttSemester
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.HjelpBtn);
+            this.Controls.Add(this.ValideringsInfoLbl);
+            this.Controls.Add(this.ValideringsTextBox);
+            this.Controls.Add(this.GodkjennNyttSemesterLbl);
+            this.Controls.Add(this.textBox1);
             this.Name = "NyttSemester";
             this.Size = new System.Drawing.Size(1008, 627);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label GodkjennNyttSemesterLbl;
+        private System.Windows.Forms.TextBox ValideringsTextBox;
+        private System.Windows.Forms.Label ValideringsInfoLbl;
+        private System.Windows.Forms.Button HjelpBtn;
     }
 }
--- adminPanel/adminPanel/NyttSemester.cs
+++ adminPanel/adminPanel/NyttSemester.cs
@@ -15,6 +15,46 @@ namespace adminPanel
         public NyttSemester()
         {
             InitializeComponent();
+            GodkjennNyttSemesterLbl.MouseDown += new MouseEventHandler(GodkjennNyttSemesterLbl_MouseDown);
+            textBox1.AllowDrop = true;
+            textBox1.DragEnter += new DragEventHandler(textBox1_DragEnter);
+            textBox1.DragDrop += new DragEventHandler(textBox1_DragDrop);
+            ValideringsTextBox.AllowDrop = true;
+            ValideringsTextBox.DragEnter += new DragEventHandler(ValideringsTextBox_DragEnter);
+            ValideringsTextBox.DragDrop += new DragEventHandler(ValideringsTextBox_DragDrop);
+        }
+        private void GodkjennNyttSemesterLbl_MouseDown(object sender,MouseEventArgs e)
+        {
+            DoDragDrop(GodkjennNyttSemesterLbl.Text, DragDropEffects.Copy);
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text)) e.Effect = DragDropEffects.Copy;
+            else e.Effect = DragDropEffects.None;
+        }
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            textBox1.Text = (String)e.Data.GetData(DataFormats.Text);
+        }
+        private void ValideringsTextBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text)) e.Effect = DragDropEffects.Copy;
+            else e.Effect = DragDropEffects.None;
+        }
+        private void ValideringsTextBox_DragDrop(object sender, DragEventArgs e)
+        {
+            ValideringsTextBox.Text = (String)e.Data.GetData(DataFormats.Text);
+        }
+
+        private void HjelpBtn_Click(object sender, EventArgs e)
+        {
+            String hjelpetekst = "En ny vurdering vil plassere eldre vurderingshistorikk i en ny tabell." +
+                "Tabellnavnet velger du i tekstboksen nedenfor." +
+                "For å godkjenne handlingen om å starte en ny semestervurdering må du dra ordet godkjenn over i godkjenningstekstboksen.";
+            //plus symbolene gir linjeskift i messagebox
+            MessageBox.Show(hjelpetekst,"Hjelp", MessageBoxButtons.OK);
+            //Parameter nr 2 setter tittelen til messagebox og parameter 3 legger til en ok knapp
         }
     }
 }
