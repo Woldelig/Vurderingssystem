5adb0ec4e475625881518d9d8ed9dce68ea92c7f
Woldelig <christoffer.wold@gmail.com>
2018-03-15 08:38:30 +0100

    la til todo for siden

--- VMS/VMS/MineFag.aspx.cs
+++ VMS/VMS/MineFag.aspx.cs
@@ -15,6 +15,17 @@ namespace VMS
             {
                 Response.Redirect("velkomstside.aspx", true);
             }
+
+            /*
+             TODO
+             1. Hente ut fag
+             2. Legge til fag i knapper
+             3. Lage en session variabel som blir hentet når man trykker på knappen
+             4. knappen videresender deg til fagsiden hvor vi henter ut den nye session variabelen og setter fagsiden til den fagkoden
+             5. Legg knappene i to kolonner
+             6. Design knappene store og pene muligens trykkbare paneler?
+             
+             */
         }
     }
 }
