0bfdc62eb1627f19e0b6e3f20721e5d8cf5805ab
Woldelig <christoffer.wold@gmail.com>
2018-03-17 16:08:20 +0100

    oppdaterte litt logikk på vurderingsskjema så man fyller ut skjema for den fagkoden man klikket på

--- VMS/VMS/MineVurderinger.aspx.cs
+++ VMS/VMS/MineVurderinger.aspx.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -14,6 +16,73 @@ namespace VMS
             if (Session["logginn"] == null || Session["studentID"] == null)
             {
                 Response.Redirect("velkomstside.aspx", true);
+            }
+            
+            Database db = new Database();
+            String sql = "SELECT COUNT(*) FROM student as s, fag, foreleser as f WHERE s.studentid = @Studentid and s.studieretning = fag.studieretning and f.foreleserid = fag.foreleserid";
+            var cmd = db.SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@Studentid", Session["studentID"].ToString());
+            db.OpenConnection();
+            MySqlDataReader leser = cmd.ExecuteReader();
+            leser.Read();
+            int antallRader = leser.GetInt32(0);
+            leser.Close();
+            db.CloseConnection();
+
+
+            sql = "SELECT fag.fagkode, fag.fagnavn, CONCAT(f.fornavn, ' ', f.etternavn) FROM student as s, fag, foreleser as f WHERE s.studentid = @Studentid and s.studieretning = fag.studieretning and f.foreleserid = fag.foreleserid";
+            cmd = db.SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@Studentid", Session["studentID"].ToString());
+            db.OpenConnection();
+            leser = cmd.ExecuteReader();
+            String[,] faginfo = new String[antallRader, 3];
+
+            int arrayIndexTilsvarerRadIdb = 0;
+            while (leser.Read())
+            {
+                faginfo[arrayIndexTilsvarerRadIdb, 0] = leser.GetString(0);
+                faginfo[arrayIndexTilsvarerRadIdb, 1] = leser.GetString(1);
+                faginfo[arrayIndexTilsvarerRadIdb, 2] = leser.GetString(2);
+                arrayIndexTilsvarerRadIdb++;
+            }
+            leser.Close();
+            db.CloseConnection();
+
+            StringBuilder sb = new StringBuilder();
+            int spanNr = 1;
+
+            //I denne for loopen blir det laget rader med klikkbare bokser som inneholder fagkode, fagnavn og foreleser navn
+            for (int i = 0; i < antallRader; i++)
+            {
+                String span1 = "FagkodeLbl" + spanNr;
+                String span2 = "FagnavnLbl" + spanNr;
+                String span3 = "ForeleserLbl" + spanNr;
+
+                sb.AppendFormat(
+                    "<div class='Row'>" +
+                        "<div class='col-md-4'>" +
+                            "<div class='divKnappBorder'>" +
+                                "<a href='Vurderingsskjema.aspx?{6}' style='text-decoration: none'>" +
+                                    "<div>" +
+                                        "<span ID='{0}' style='color:Black;font-weight:bold;'>{3}</span><br />" +
+                                        "<span ID='{1}' style='color:Black'>{4}</span><br />" +
+                                        "<span ID='{2}' style='color:Black'>{5}</span><br />" +
+                                    "</div>" +
+                                "</a>" +
+                            "</div >" +
+                        "</div >" +
+                    "</div >" +
+                    "<br />" +
+                    "<br />" +
+                    "<br />" +
+                    "<br />" +
+                    "<br />"
+                    , span1, span2, span3, "Fagkode: " + faginfo[i, 0], "Fagnavn: " + faginfo[i, 1], "Foreleser: " + faginfo[i, 2], faginfo[i, 0]);
+                //span1-3 angir span navn, de får et høyere nr per loop. [i,0] er fagkode for første rad [i,1] er fagnavn og [i,2] er foreleser navn
+                
+
+                //lit er forkortelsen for literal kontroll vi skriver ut stringbuilderen sin tekst til
+                lit.Text = sb.ToString();
             }
         }
     }
