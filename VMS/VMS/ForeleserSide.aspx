8f46effa1c6a038170d90ba0f5d70e22d7e64e5f
Woldelig <christoffer.wold@gmail.com>
2018-04-09 14:57:39 +0200

    layout og logikk

---
+++ VMS/VMS/ForeleserSide.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ForeleserSide.aspx.cs" Inherits="VMS.ForeleserSide" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="foreleserLbl" runat="server" Font-Bold="True" Font-Size="XX-Large" Text=""></asp:Label>
+    <br />
+    <br />
+    <br />
+    <div class="Row">
+        <div class="col-md-4">
+            <asp:Label ID="fakultetLbl" runat="server" Text="Fakultet: Handelshøyskolen"></asp:Label></div>
+        <div class="col-md-4">
+            <asp:Label ID="studieretningLbl" runat="server" Text="Linje: It og Informasjonssytemer"></asp:Label></div>
+    </div>
+    <div class="Row">
+        <table Class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Fagkode</th>
+                    <th>Fagnavn</th>
+                    <th>Fakultet</th>
+                </tr>
+            </thead>
+            <tbody id="tableBody" runat="server">
+                
+            </tbody>
+        </table>
+    </div>
+    <br />
+    <br />
+    <br />
+</asp:Content>
---
+++ VMS/VMS/ForeleserSide.aspx.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VMS
+{
+    public partial class ForeleserSide : System.Web.UI.Page
+    {
+        private Database db = new Database();
+        private String sidensForleserId = "666";
+
+        /*
+         * Setter en standard foreleserId så det alltid vil vises noe informasjon
+         */
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Hvis noen blir redirected til forelesersiden med et parameter vil forelsersiden bytte om fagkoden til parameteret ved hjelp av en stringQuery
+            String uformatertQueryString = Request.Url.Query;
+            String formatertQueryString = uformatertQueryString.Replace("?", String.Empty);
+            //StringQuery inneholder et spørsmålstegn som vi her fjerner
+
+            if (formatertQueryString != "" || formatertQueryString == null)
+            {
+                sidensForleserId = formatertQueryString;
+            }
+
+            //Henter ut navn, fagkode, fagnavn, fakultet, studieretning
+            String query = "SELECT CONCAT(f.fornavn, ' ', f.etternavn) as navn, fag.fagkode, fag.fagnavn, fag.studieretning, s.fakultet from foreleser as f, fag, studier as s WHERE f.foreleserid = @SidensForeleser AND fag.foreleserid = f.foreleserid AND fag.studieretning = s.studieretning";
+            var cmd = db.SqlCommand(query);
+            cmd.Parameters.AddWithValue("@SidensForeleser", sidensForleserId);
+
+            db.OpenConnection();
+
+            using (MySqlDataReader leser = cmd.ExecuteReader())
+            {
+                if (!leser.HasRows)
+                {
+                    //Hvis sql feilet/ugyldig parameter til siden blir man sendt til default.aspx
+                    Server.Transfer("Default.aspx");
+                }
+            }
+        }
+    }
+}
