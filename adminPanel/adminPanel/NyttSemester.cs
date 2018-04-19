16fa1c9739698ae8e0089afb93babde622b5fc74
Woldelig <christoffer.wold@gmail.com>
2018-04-19 17:43:06 +0200

    NyttSemester.cs

--- adminPanel/adminPanel/NyttSemester.cs
+++ adminPanel/adminPanel/NyttSemester.cs
@@ -19,7 +19,10 @@ namespace adminPanel
             FeilmeldingLbl.Text = "";
             AvsluttVurderingFeilmeldingLbl.Text = "";
 
-            //Event handlers blir definert under. Mye lettere å håndtere de her enn i design. Spesielt hvis man skal endre navn.
+            /*
+             * Her legges vi inn eventhandlere med kode, dette kan også gjøres i designdelen.
+             * Når man legges inn drag and drop som kode, må man huske på å sette AllowDrop til true
+             */
             NyTabellGodkjennNyttSemesterLbl.MouseDown += new MouseEventHandler(NyTabellGodkjennNyttSemesterLbl_MouseDown);
             NyTabellIkkeGodkjennNyttSemesterLbl.MouseDown += new MouseEventHandler(NyTabellIkkeGodkjennNyttSemesterLbl_MouseDown);
             GodkjennAvsluttVurderingLbl.MouseDown += new MouseEventHandler(GodkjennAvsluttVurderingLbl_MouseDown);
@@ -45,6 +48,12 @@ namespace adminPanel
             tooltip1.SetToolTip(this.NyTabellIkkeGodkjennNyttSemesterLbl, "Dra meg over til valideringsboksen for å godkjenne ikke handlingen.");
             tooltip1.SetToolTip(this.AvsluttVurderingBtn, "Avslutter pågående vurdering.");
         }
+
+        /*
+         * Under legges det til metoder for at drag and drop skal fungere.
+         * Disse metodene heter MouseDown, DragEnter og DragDrop.
+         * Innenfor disse metodene må man legge til effekter og logikk
+         */
         private void NyTabellGodkjennNyttSemesterLbl_MouseDown(object sender,MouseEventArgs e)
         {
             DoDragDrop(NyTabellGodkjennNyttSemesterLbl.Text, DragDropEffects.Copy);
@@ -82,16 +91,32 @@ namespace adminPanel
 
         private void HjelpBtn_Click(object sender, EventArgs e)
         {
+            /*
+             * Hjelpeknappen åpner en messagebox som inneholder hjelpetekst 
+             * for hvordan man skal bruke funksjonaliteten.
+             * 
+             * I parameteret til MessageBox.Show er det mulighet til å legge til
+             * MessageBoxButtons.OKcancel. Dette er egentlig måten vi ville ha validering 
+             * på, men vi endte opp og bruke drag and drop metoden fordi dette sto i teksten 
+             * til semesteroppgaven
+             */
             String hjelpetekst = "En ny vurdering vil plassere eldre vurderingshistorikk i en ny tabell." +
                 "Tabellnavnet velger du i tekstboksen nedenfor." +
                 "For å godkjenne handlingen om å starte en ny semestervurdering må du dra ordet godkjenn over i godkjenningstekstboksen.";
             //plus symbolene gir linjeskift i messagebox
             MessageBox.Show(hjelpetekst,"Hjelp", MessageBoxButtons.OK);
-            //Parameter nr 2 setter tittelen til messagebox og parameter 3 legger til en ok knapp
         }
 
         private void StartNyttSemesterBtn_Click(object sender, EventArgs e)
         {
+            /*
+             * Navnet til denne knappen er misvisende, da den ikke gjør dette lenger
+             * 
+             * Denne metoden lar deg mellomlagre vurderinger uten og avslutte den.
+             * Dette vil gi en administrator mulighet til å lagre vurdering_høst_2018 
+             * til en egen tabell. Dette gir mulighet for å sammenligne semestere direkte
+             * opp mot hverandre
+             */
             FeilmeldingLbl.ForeColor = Color.Red;
             //Under sjekker vi først at tekstboksen ikke er tom eller bare har whitespace, deretter sjekker vi valideringstekstboksen
             if (String.IsNullOrWhiteSpace(TabellNavnTextbox.Text))
@@ -106,8 +131,10 @@ namespace adminPanel
             {
                 Database db = new Database();
                 var cmd = db.SqlCommand("SELECT * FROM information_schema.tables WHERE table_schema = 'vurderingssystem' AND table_name = @tabell LIMIT 1;");
-                //Setningen over sjekker om tabellen eksisterer. Man kan ikke bruke en vanlig select * from tabellnavn
-                //grunnen til dette er at hvis tabellen ikke eksisterer vil mysql gi en feilmelding
+                /*
+                 * Setningen over sjekker om tabellen eksisterer. Man kan ikke bruke en vanlig select * from tabellnavn
+                 * grunnen til dette er at hvis tabellen ikke eksisterer vil mysql gi en feilmelding.
+                 */
                 cmd.Parameters.AddWithValue("@tabell", TabellNavnTextbox.Text);
                 db.OpenConnection();
                 MySqlDataReader reader = cmd.ExecuteReader();
@@ -119,7 +146,7 @@ namespace adminPanel
                 }
                 db.CloseConnection();
 
-
+                //Her setter vi inn navnet på prosedyren
                 cmd = db.SqlCommand("lagre_pågående_evaluerings_resultater");
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@ny_tabell", TabellNavnTextbox.Text).Direction = ParameterDirection.Input;
@@ -140,6 +167,16 @@ namespace adminPanel
 
         private void AvsluttVurderingBtn_Click(object sender, EventArgs e)
         {
+            /*
+             * Her kaller vi på en prosedyre som avslutter nåværende vurdering.
+             * Dette skjer ved hjelp av en prosedyre som henter ut alle radene i tabellen
+             * pågåendevurdering og setter de inn i vurderingshistorikk tabellen. Deretter 
+             * blir pågåendevurdering tabellen tømt og vi er nå klare for å starte en ny vurdering.
+             * 
+             * Når denne operasjonen er utført vil vurderingsdataene til vurderingene som er utført
+             * i denne perioden bli lagt inn sammen med resten av dataene så den kan vises med diagramer
+             * enten i adminpanelet her eller på nettsiden
+             */
             AvsluttVurderingFeilmeldingLbl.ForeColor = Color.Red;
             if (GodkjennAvsluttVurderingTextbox.Text != "Godkjenn")
             {
