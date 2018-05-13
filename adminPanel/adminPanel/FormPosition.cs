af79f0674a3958015efe5fdbc235600d4cb552dc
ZuperNova <trugunn_css@hotmail.com>
2018-05-13 11:18:57 +0200

    kommentering og fjernet unødvendig direktiver
    FormPosition

--- adminPanel/adminPanel/FormPosition.cs
+++ adminPanel/adminPanel/FormPosition.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace adminPanel
 {
+    // En egen aksessor og mutator for å hente ut posisjonen på selve formen.
     class FormPosition
     {
         static private Point pos;
 
         static public Point Pos
         {
+            // Get-metoden
             get
             {
                 return pos;
             }
-
+            // Set-metoden
             set
             {
                 pos = value;
